66a622c48d1584f40c2aa184dd32add44e784a0f
okhi3945 <yhoka@naver.com>
2024-03-18 20:42:13 +0900

    최대공약수와 최소공배수
    최대공약수와 최소공배수

--- codingTest.java
+++ codingTest.java
@@ -1,15 +1,22 @@
 class Solution {
-    public String solution(String s) {
-        String answer = "";
-        if((s.length()%2) == 0){
-            answer = String.valueOf(s.charAt(s.length()/2-1)) + String.valueOf(s.charAt(s.length()/2));
-        }else{
-            answer = String.valueOf(s.charAt(s.length()/2));
+    public int[] solution(int n, int m) {
+        int[] answer = new int[2];
+        
+        int a = 1;   
+        for(int i =1;i<n+1;i++){ //두 값과 모두 나누어 떨어지는 값 찾기(최대 공약수)
+            if(n%i == 0 && m%i == 0){
+                a = i;
+            }
         }
+        answer[0] = a;
+        answer[1] = n/a*m;
+
         return answer;
     }
+
+
     public static void main(String[] args) {
         Solution solution = new Solution();
-        solution.solution("abcded");
+        solution.solution(6,16);
     }
 }
