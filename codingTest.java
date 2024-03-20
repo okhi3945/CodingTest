7379b6e0324d18b90d4d665c9f768f53b66b7478
okhi3945 <yhoka@naver.com>
2024-03-20 14:36:24 +0900

    이상한 문자 만들기
    이상한 문자 만들기

--- codingTest.java
+++ codingTest.java
@@ -1,22 +1,29 @@
 class Solution {
-    public int[] solution(int n, int m) {
-        int[] answer = new int[2];
-        
-        int a = 1;   
-        for(int i =1;i<n+1;i++){ //두 값과 모두 나누어 떨어지는 값 찾기(최대 공약수)
-            if(n%i == 0 && m%i == 0){
-                a = i;
+    public String solution(String s) {
+        String answer = "";
+        int cnt = 0; //cnt로 공백시 마다 카운트 초기화
+        for (int i = 0; i < s.length(); i++) {
+
+            if (s.charAt(i) != ' ') {
+                if (cnt % 2 == 0) {
+                    answer += String.valueOf(s.charAt(i)).toUpperCase();
+                    cnt++;
+                } else {
+                    answer += String.valueOf(s.charAt(i)).toLowerCase(); //홀수일때 소문자로 변환시켜주기
+                    cnt++;
+                }
+            } else {
+                answer += " ";
+                cnt = 0;
             }
         }
-        answer[0] = a;
-        answer[1] = n/a*m;
 
         return answer;
     }
 
-
     public static void main(String[] args) {
         Solution solution = new Solution();
-        solution.solution(6,16);
+        // solution.solution("try hello world");
+        System.out.println(solution.solution("tryaaworl  dabc"));
     }
 }
