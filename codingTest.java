310e4e8cc52c08fe3450f3c935ef292a0378e335
okhi3945 <yhoka@naver.com>
2024-03-26 19:04:02 +0900

    2016년
    2016년

--- codingTest.java
+++ codingTest.java
@@ -1,50 +1,35 @@
 class Solution {
-    public String[] solution(int n, int[] arr1, int[] arr2) {
-        String[] answer = new String[n];
-        char[][] a = new char[n][];
-        char[][] b = new char[n][];
-        for (int i = 0; i < n; i++) {
-            //Integer 클래스의 toBinaryString() 메서드를 사용하여 정수를 이진수(문자열)로 변환
-            //변환된 이진수의 길이를 n으로 만들기 위해서 앞에 부족한 만큼 0 추가해주기(repeat() 함수 사용)
-            //toCharArray() 사용하여 char 배열에 값 넣어주기
-            if (Integer.toBinaryString(arr1[i]).length() < n) {
-                a[i] = ("0".repeat(n - Integer.toBinaryString(arr1[i]).length()) + Integer.toBinaryString(arr1[i]))
-                        .toCharArray();
-            } else {
-                a[i] = Integer.toBinaryString(arr1[i]).toCharArray();
-            }
-            if (Integer.toBinaryString(arr2[i]).length() < n) {
-                b[i] = ("0".repeat(n - Integer.toBinaryString(arr2[i]).length()) + Integer.toBinaryString(arr2[i]))
-                        .toCharArray();
-            } else {
-                b[i] = Integer.toBinaryString(arr2[i]).toCharArray();
-            }
+    public String solution(int a, int b) {
+        String answer = "";
+        //2016년 1월 1일 금요일 -> week 배열 FRI부터 시작
+        //윤년 -> 2월 29일까지
+        String[] week = {"FRI","SAT","SUN","MON","TUE","WED","THU"};
+        int[] month = {31,29,31,30,31,30,31,31,30,31,30,31};
+        int day = 0;
+        
+        //a월까지의 일수 다 더해주기
+        for(int i = 0;i<a-1;i++){
+            day += month[i];
         }
 
-        for (int i = 0; i < n; i++) {
-            String temp = "";
-            for (int j = 0; j < n; j++) {
-                //a[i][j] - '0'을 해주어서 a의 각 배열의 값을 문자에서 정수로 변환
-                //두 정수의 값을 합한 값이 1이 넘어가면 #으로 표시
-                if((a[i][j] - '0') + (b[i][j] - '0') >= 1){
-                    temp += "#";
-                }else{
-                    temp += " ";
-                }
-            }
-            answer[i] = temp;
-        }
-        for(int i = 0;i<answer.length;i++){
-            System.out.println(answer[i]);
+        //day%7하면 각 월의 1일 요일이 나오고
+        //+ b%7로 일수에 따른 요일 계산
+        //day%7 + b%7 값이 7이 넘어가는 것을 방지하도록 한번 더 %7해서 요일만 알아내고
+        //배열을 "FRI"부터 시작하였기 때문에 1월 1일의 경우 1이 나오는데, 자기자신을 빼고, 
+        //배열의 0번째 값인 FRI를 가져오기 위해 - 1을 해준다.
+        int p = (day%7 + b%7)%7 - 1;
+        if(p < 0){
+            p += 7;
         }
 
+        System.out.println(p);
+        answer = week[p];
+        
         return answer;
     }
 
     public static void main(String[] args) {
         Solution solution = new Solution();
-        int[] a = { 46, 33, 33 ,22, 31, 50 };
-        int[] b = { 27 ,56, 19, 14, 14, 10 };
-        System.out.println(solution.solution(6, a, b));
+        System.out.println(solution.solution( 1, 1));
     }
 }
