ef82df6c9dfb676bc1909fce274c896a314dc2cc
okhi3945 <yhoka@naver.com>
2024-04-04 19:51:22 +0900

    최솟값 만들기
    최솟값 만들기

--- codingTest.java
+++ codingTest.java
@@ -1,53 +1,47 @@
 import java.util.*;
 
 class Solution {
-    public int solution(String s) {
+    public int solution(int[] A, int[] B) {
         int answer = 0;
+        // 제일 작은 숫자와 제일 큰 숫자를 곱해야겠네
+        // 효율성 테스트가 빡쌤 아마도 for문을 2번써서 그런 것 같음
+        // for (int i = 0; i < A.length; i++) {
+        // for (int j = i + 1; j < A.length; j++) {
+        // if (A[j] < A[i]) {
+        // int tmp = 0;
+        // tmp = A[j];
+        // A[j] = A[i];
+        // A[i] = tmp;
+        // }
+        // if (B[i] < B[j]) {
+        // int tmp = 0;
+        // tmp = B[j];
+        // B[j] = B[i];
+        // B[i] = tmp;
+        // }
+        // }
+        // }
+        ArrayList<Integer> list = new ArrayList<>();
+        ArrayList<Integer> Blist = new ArrayList<>();
+        for (int i = 0; i < A.length; i++) {
+            list.add(A[i]);
+            Blist.add(B[i]);
+        }
+        list.sort(Comparator.naturalOrder());
+        Blist.sort(Comparator.reverseOrder());
 
-        // aaabbaccccabba
-        // a -> aa -> aaa -> aaabb -> aaabba -> aaabbac -> aaabbacc
-        // c -> cc -> cca -> ccab
-        // b -> ba
-        ArrayList<String> list = new ArrayList<>(); //분리된 문자열을 저장해둘 리스트
-        String a = "";
-        // cor = x와 같은 글자가 나온 횟수
-        // incor = x가 아닌 다른 글자들이 나온 횟수
-        int cor = 0, incor = 0;
-        for (int i = 0; i < s.length(); i++) {
-            a += String.valueOf(s.charAt(i));
-            System.out.println(a);
-            for (int j = 0; j < a.length(); j++) {
-                if (a.charAt(0) == a.charAt(j)) {
-                    cor++;
-                    System.out.println("cor " + cor + " " + a);
-                } else {
-                    incor++;
-                    System.out.println("incor " + incor + " " + a);
-                }
-                if (incor == cor) {
-                    System.out.println(incor + " " + cor + " " + i + " " + a);
-                    System.out.println(a + " incor == cor");
-                    list.add(a);
-                    a = "";
-                }
-            }
-            cor = 0;
-            incor = 0;
+        
+        for(int i = 0;i<list.size();i++){
+            answer += (list.get(i) * Blist.get(i));
         }
-        System.out.println("a : " + a);
-        if (!a.isEmpty()) {
-            list.add(a);
-        }
-        for (String hi : list) {
-            System.out.println(hi);
-        }
-        answer = list.size();
-
+        
         return answer;
     }
 
     public static void main(String[] args) {
         Solution solution = new Solution();
-        System.out.println(solution.solution("aaabbaccccabba"));
+        int[] a = { 5, 100, 5, 350, 37, 49 };
+        int[] b = { 80, 60, 90, 7, 190, 20 };
+        System.out.println(solution.solution(a, b));
     }
 }
