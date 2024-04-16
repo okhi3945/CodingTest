6403b5012e4dffb466694d72eb91c725e33dd5a9
okhi3945 <yhoka@naver.com>
2024-04-16 18:41:57 +0900

    기사단원의 무기
    기사단원의 무기

--- codingTest.java
+++ codingTest.java
@@ -1,24 +1,42 @@
 class Solution {
-    public int solution(int n) {
-        // Integer.bitCount 함수를 통해 n을 이진수로 만들었을 때 1의 개수를 b에 저장한다.
-        int b = Integer.bitCount(n);
+    public int solution(int number, int limit, int power) {
+        int answer = 0;
+        int plus = 0;
 
-        while(true){
-            // 무한 루프를 돌며 n의 값을 증가시키기
-            n++;
-            
-            // n의 값을 증가시키다가 증가된 n의 이진수 표현 1의 개수가 b와 같을 때 반환
-            if(b == Integer.bitCount(n)){
-                return n;
+        // 1부터 number까지 약수의 개수 구하기
+        for (int j = 1; j <= number; j++) {
+
+            // 약수의 개수 값을 담을 cnt값
+            int cnt = 0;
+
+            // 약수의 개수를 계산할 for문
+            for (int i = 1; i * i <= j; i++) {
+
+                //제곱근이면 한쌍의 약수 밖에 나올 수 없기 때문에 cnt에 1더하기
+                if (i * i == j) {
+                    cnt++;
+                } else if (j % i == 0) { 
+                    //제곱근이 아니면 이 값은 항상 쌍을 이루기 때문에 그 쌍까지 더해서 +2 해주기
+                    cnt += 2;
+                }
             }
+
+            // 약수의 갯수가 제한 수치 limit값 보다 크면 무기의 공격력은 power로 제한된다.
+            if(cnt > limit){
+                plus = power;
+            }else { //제한 수치를 넘지 않으면 약수의 개수는 그대로 공격력이 되어 무게가 된다.
+                plus = cnt;
+            }
+            //answer에 무게를 더해주며 반복한다.
+            answer+=plus;
         }
+
+        return answer;
     }
-
-
 
     public static void main(String[] args) {
         Solution solution = new Solution();
-        System.out.println(solution.solution(78));
+        System.out.println(solution.solution(5, 3, 2));
 
     }
 }
