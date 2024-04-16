96da97182063c886b3b14c77a2174228a6d5913e
okhi3945 <yhoka@naver.com>
2024-04-16 18:01:24 +0900

    다음 큰 숫자
    다음 큰 숫자

--- codingTest.java
+++ codingTest.java
@@ -1,29 +1,24 @@
 class Solution {
     public int solution(int n) {
-        int answer = 1; //자기 자신 미리 더하고 시작
+        // Integer.bitCount 함수를 통해 n을 이진수로 만들었을 때 1의 개수를 b에 저장한다.
+        int b = Integer.bitCount(n);
 
-        // 연속되는 수 더해서 n이 되려면 n/2가 마지노선임
-        for(int i = 1;i<=n/2;i++){
-            int sum = 0;
-            int k = i;
-
-            //sum이 n보다 작은 동안 while문 돌림
-            while(sum<n){
-                sum+=k;
-                k++;
-            }
-            //while문을 마친 sum의 값이 n과 같으면 answer+=1
-            if(sum==n){
-                answer+=1;
+        while(true){
+            // 무한 루프를 돌며 n의 값을 증가시키기
+            n++;
+            
+            // n의 값을 증가시키다가 증가된 n의 이진수 표현 1의 개수가 b와 같을 때 반환
+            if(b == Integer.bitCount(n)){
+                return n;
             }
         }
-
-        return answer;
     }
+
+
 
     public static void main(String[] args) {
         Solution solution = new Solution();
-        System.out.println(solution.solution(15));
+        System.out.println(solution.solution(78));
 
     }
 }
