1a9bc66f0aeddb673c697509da76fe8b71bb6690
okhi3945 <yhoka@naver.com>
2024-04-17 19:37:16 +0900

    피보나치 수
    피보나치 수

--- codingTest.java
+++ codingTest.java
@@ -1,42 +1,24 @@
 class Solution {
-    public int solution(int number, int limit, int power) {
-        int answer = 0;
-        int plus = 0;
+    public int solution(int n) {
+        int[] dp = new int[n + 1];
+        dp[0] = 0;
+        dp[1] = 1;
 
-        // 1부터 number까지 약수의 개수 구하기
-        for (int j = 1; j <= number; j++) {
-
-            // 약수의 개수 값을 담을 cnt값
-            int cnt = 0;
-
-            // 약수의 개수를 계산할 for문
-            for (int i = 1; i * i <= j; i++) {
-
-                //제곱근이면 한쌍의 약수 밖에 나올 수 없기 때문에 cnt에 1더하기
-                if (i * i == j) {
-                    cnt++;
-                } else if (j % i == 0) { 
-                    //제곱근이 아니면 이 값은 항상 쌍을 이루기 때문에 그 쌍까지 더해서 +2 해주기
-                    cnt += 2;
-                }
-            }
-
-            // 약수의 갯수가 제한 수치 limit값 보다 크면 무기의 공격력은 power로 제한된다.
-            if(cnt > limit){
-                plus = power;
-            }else { //제한 수치를 넘지 않으면 약수의 개수는 그대로 공격력이 되어 무게가 된다.
-                plus = cnt;
-            }
-            //answer에 무게를 더해주며 반복한다.
-            answer+=plus;
+        // 피보나치 수열은 첫째 항과 둘째 항이 1이고,
+        // 그 이후의 항은 바로 앞의 두 항을 더한 수로 이루어진 수열임 ex) 0,1,1,2,3,5,8,,,
+        for (int i = 2; i <= n; i++) {
+            // n의 범위가 100,000이기 때문에 오버플로우 발생
+            // ex) n이 47일 때의 피보나치 수 : 2,971,215,073 -> 32bit 정수(int)의 범위 넘음
+            // 100,000번째 피본치 수는 자릿수가 20,000을 넘어가고, 64bit 정수(long)의 범위 넘음
+            //그렇기 때문에 오버플로우가 일어나지 않게 1234567을 나눈 나머지 값으로 계산
+            dp[i] = (dp[i - 1] + dp[i - 2]) % 1234567;
         }
-
-        return answer;
+        return dp[n];
     }
 
     public static void main(String[] args) {
         Solution solution = new Solution();
-        System.out.println(solution.solution(5, 3, 2));
+        System.out.println(solution.solution(100000));
 
     }
 }
