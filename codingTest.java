52bdd36795ee32c8857fcf66ea2aa7d80067406d
okhi3945 <yhoka@naver.com>
2024-05-07 18:29:22 +0900

    짝지어 제거하기(프로그래머스 Lv.2)
    짝지어 제거하기

--- codingTest.java
+++ codingTest.java
@@ -1,24 +1,32 @@
+import java.util.*;
 class Solution {
-    public int solution(int n) {
-        int[] dp = new int[n + 1];
-        dp[0] = 0;
-        dp[1] = 1;
+    public int solution(String s)
+    {
+        //처음에 스택 쓸 생각을 못하고 리스트나 세트로 풀려고 했었음 스택과 큐의 존재도 알아놔야겠다.
+        int answer = -1;
+        //stack을 character 타입으로 만들어서 (**Stack(Character>**)문자열의 각 자리의 문자를 스택에 삽입
+        Stack<Character> stack = new Stack<>(); 
 
-        // 피보나치 수열은 첫째 항과 둘째 항이 1이고,
-        // 그 이후의 항은 바로 앞의 두 항을 더한 수로 이루어진 수열임 ex) 0,1,1,2,3,5,8,,,
-        for (int i = 2; i <= n; i++) {
-            // n의 범위가 100,000이기 때문에 오버플로우 발생
-            // ex) n이 47일 때의 피보나치 수 : 2,971,215,073 -> 32bit 정수(int)의 범위 넘음
-            // 100,000번째 피본치 수는 자릿수가 20,000을 넘어가고, 64bit 정수(long)의 범위 넘음
-            //그렇기 때문에 오버플로우가 일어나지 않게 1234567을 나눈 나머지 값으로 계산
-            dp[i] = (dp[i - 1] + dp[i - 2]) % 1234567;
+        for(int i = 0; i<s.length();i++){
+            //스택이 비어있지 않고, 제일 꼭대기와 내가 지금 넣으려는 값이 같으면 넣지 않고 원래 있던
+            //값 삭제, 그렇지 않고 비어있다면 값 삽입
+            if(!stack.isEmpty() && stack.peek() == s.charAt(i)){
+                stack.pop();
+            }else{
+                stack.push(s.charAt(i));
+            }
         }
-        return dp[n];
+        if(stack.size() == 0){
+            answer = 1;
+        } else{
+            answer = 0;
+        }
+        return answer;
     }
 
     public static void main(String[] args) {
         Solution solution = new Solution();
-        System.out.println(solution.solution(100000));
+        System.out.println(solution.solution("cdcd"));
 
     }
 }
