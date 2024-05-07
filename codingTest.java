5bf5780535ed33fb76882cbb74a0002ea5aed83a
okhi3945 <yhoka@naver.com>
2024-05-07 19:31:55 +0900

    영어 끝말잇기(프로그래머스 Lv.2)
    영어 끝말잇기(프로그래머스 Lv.2)

--- codingTest.java
+++ codingTest.java
@@ -1,32 +1,61 @@
 import java.util.*;
-class Solution {
-    public int solution(String s)
-    {
-        //처음에 스택 쓸 생각을 못하고 리스트나 세트로 풀려고 했었음 스택과 큐의 존재도 알아놔야겠다.
-        int answer = -1;
-        //stack을 character 타입으로 만들어서 (**Stack(Character>**)문자열의 각 자리의 문자를 스택에 삽입
-        Stack<Character> stack = new Stack<>(); 
 
-        for(int i = 0; i<s.length();i++){
-            //스택이 비어있지 않고, 제일 꼭대기와 내가 지금 넣으려는 값이 같으면 넣지 않고 원래 있던
-            //값 삭제, 그렇지 않고 비어있다면 값 삽입
-            if(!stack.isEmpty() && stack.peek() == s.charAt(i)){
-                stack.pop();
-            }else{
-                stack.push(s.charAt(i));
+class Solution {
+    public int[] solution(int n, String[] words) {
+        int[] answer = {0,0};
+
+        ArrayList<String> list = new ArrayList<>();
+        //처음 나온 단어는 먼저 리스트에 넣고 생각
+        list.add(words[0]);
+        int cnt = 1;
+        for (int i = 1; i < words.length; i++) {
+            cnt++;
+            //끝말잇기 단어 리스트에 넣어두고 마지막 글자와 새로 넣으려는 글자의 첫 글자가 같은 지 확인
+            if (words[i - 1].endsWith(String.valueOf(words[i].charAt(0)))) {
+                System.out.println("같다" + words[i]);
+
+                //중복된 값 발견(contain)시 번호(cnt)와 차례 찾아서 answer에 답 넣고 break
+                if (list.contains(words[i])) {
+                    System.out.println("중복 : " + words[i]);
+                    System.out.println("번호 : " + cnt);
+                    if ((i + 1) % n == 0) {
+                        System.out.println("차례 : " + (i + 1) / n);
+                        answer[0] = cnt;
+                        answer[1] = ((i + 1) / n);
+                    } else {
+                        System.out.println("다르다 : " + (((i + 1) / n) + 1));
+                        answer[0] = cnt;
+                        answer[1] = (((i + 1) / n) + 1);
+                    }
+                    break;
+                }
+                list.add(words[i]);
+            } else {
+                System.out.println("다르다 번호 : " + cnt);
+                if ((i + 1) % n == 0) {
+                    System.out.println("차례 : " + (i + 1) / n);
+                    answer[0] = cnt;
+                    answer[1] = ((i + 1) / n);
+                } else {
+                    System.out.println("다르다 : " + (((i + 1) / n) + 1));
+                    answer[0] = cnt;
+                    answer[1] = (((i + 1) / n) + 1);
+                }
+                break;
+            }
+
+            if (cnt >= n) {
+                cnt = 0;
             }
         }
-        if(stack.size() == 0){
-            answer = 1;
-        } else{
-            answer = 0;
-        }
+
         return answer;
     }
 
     public static void main(String[] args) {
         Solution solution = new Solution();
-        System.out.println(solution.solution("cdcd"));
+        String[] s = { "tank", "kick", "know", "wheel", "land", "dream", "mother", "robot", "tank" };
+        System.out.println(solution.solution(3, s));
 
     }
 }
