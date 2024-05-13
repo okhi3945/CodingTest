2cef672ce0fc1c0181a09295854c6b109bb377d7
okhi3945 <yhoka@naver.com>
2024-05-13 22:06:41 +0900

    멀리뛰기(프로그래머스, Lv.2, 자바)
    멀리뛰기(프로그래머스, Lv.2, 자바)

--- codingTest.java
+++ codingTest.java
@@ -1,61 +1,27 @@
-import java.util.*;
-
 class Solution {
-    public int[] solution(int n, String[] words) {
-        int[] answer = {0,0};
+    public long solution(int n) {
 
-        ArrayList<String> list = new ArrayList<>();
-        //처음 나온 단어는 먼저 리스트에 넣고 생각
-        list.add(words[0]);
-        int cnt = 1;
-        for (int i = 1; i < words.length; i++) {
-            cnt++;
-            //끝말잇기 단어 리스트에 넣어두고 마지막 글자와 새로 넣으려는 글자의 첫 글자가 같은 지 확인
-            if (words[i - 1].endsWith(String.valueOf(words[i].charAt(0)))) {
-                System.out.println("같다" + words[i]);
-
-                //중복된 값 발견(contain)시 번호(cnt)와 차례 찾아서 answer에 답 넣고 break
-                if (list.contains(words[i])) {
-                    System.out.println("중복 : " + words[i]);
-                    System.out.println("번호 : " + cnt);
-                    if ((i + 1) % n == 0) {
-                        System.out.println("차례 : " + (i + 1) / n);
-                        answer[0] = cnt;
-                        answer[1] = ((i + 1) / n);
-                    } else {
-                        System.out.println("다르다 : " + (((i + 1) / n) + 1));
-                        answer[0] = cnt;
-                        answer[1] = (((i + 1) / n) + 1);
-                    }
-                    break;
-                }
-                list.add(words[i]);
-            } else {
-                System.out.println("다르다 번호 : " + cnt);
-                if ((i + 1) % n == 0) {
-                    System.out.println("차례 : " + (i + 1) / n);
-                    answer[0] = cnt;
-                    answer[1] = ((i + 1) / n);
-                } else {
-                    System.out.println("다르다 : " + (((i + 1) / n) + 1));
-                    answer[0] = cnt;
-                    answer[1] = (((i + 1) / n) + 1);
-                }
-                break;
-            }
-
-            if (cnt >= n) {
-                cnt = 0;
-            }
+        //한번 가는데 1칸 또는 2칸을 뛸 수 있음
+        //그렇다면 n번째까지 점프해서 가는 경우의 수는 n-1번째 칸에서 1칸 뛴 값
+        //과 n-2번째 칸에서 2칸 뛰어서 오는 값의 합 밖에 존재하지 않음
+        //동적 계획법의 점화식을 사용함
+        //dp[n] = dp[n-1] + dp[n]
+        //n번째 칸의 값을 구하려면 n-1, n-2번째 칸의 값을 더하면 된다는 것을 알고 점화식을 사용할 줄 알아야함
+        int[] dp = new int[2001];
+        dp[1] = 1; //1번째 칸에 가는 경우의 수 1개(1칸)
+        dp[2] = 2; //2번째 칸에 갈 수 있는 경우의 수는 2개(1칸+1칸, 2칸)
+        //dp[n]을 구해야함 -> n번째 칸으로 가는 방법의 개수
+        for(int i = 3;i<=n;i++){
+            dp[i] = ((dp[i-1] + dp[i-2]) % 1234567);
         }
-
-        return answer;
+        
+        return dp[n];
+        
     }
 
     public static void main(String[] args) {
         Solution solution = new Solution();
-        String[] s = { "tank", "kick", "know", "wheel", "land", "dream", "mother", "robot", "tank" };
-        System.out.println(solution.solution(3, s));
+        System.out.println(solution.solution(4));
 
     }
 }
